4036fbae8481e8bbd7ab29d1da5b9bcbc7f32f8a
Firemixx <maksmasur3@gmail,com>
2025-09-11 15:34:30 +0300

    Updated version of bot for sync on update without 100000000000 syncs

--- main.py
+++ main.py
@@ -5,107 +5,119 @@ from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions
 from discord import Member
 from discord.utils import get
+import logging
+from log_config import configurated_logging
 
+
+log=logging.getLogger()
 load_dotenv() # load all the variables from the env file
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True           
-intents.presences = False  #Intests for what I waste 4 hourssss...
+intents.presences = False  #разрешения, из-за которых я угробил четыре часаааа...
 bot = commands.Bot(intents=intents)
 
+guildlist_raw = os.getenv("guilds")
+guildslist = list(map(int, guildlist_raw.split(",")))
+endedOrNote=True
 
-guildslist=[]#Servers on what will work bot
-lenofGlist=len(guildslist)
 
+async def sync_roles():
+    await configurated_logging(level=logging.INFO)
+    log.info("Старт проверки и синхронизации")
+    guilds = [bot.get_guild(g) for g in guildslist if bot.get_guild(g)]
+    endedOrNote=False
+    for guild in guilds:
+        for member in guild.members:
+            log.info(f"Чекинг {member.name} на {guild.name}")
+            if member.bot:
+                continue
+                
+            memberRoles=member.roles
+            for r in memberRoles:
+                rname=r.name
+                log.info(f'имя пользователя:{rname}')
+                if rname!='падаван':
+                    log.warning(f'Имя роли что была пропущена:{r}')
+                    continue
+                if rname=='падаван':
+                    for g in guilds:
+                        
+                        user=g.get_member(member.id)
+                        # Цикл по всем серверам и передача ролей на них
+                        role = discord.utils.get(g.roles, name=rname)
+                        log.warning(f'Готовая роль для назначения:{role}')
+                        if role:
+                            try:
+                                await user.add_roles(role)
+                                log.info(f"{user.name} получил {rname} на сервере: {guild.name}")
+                            except discord.Forbidden:
+                                log.error(f"Нет прав выдать {rname} на {guild.name}")
+                            else:
+                                if role == None:
+                                    log.fatal(f"{rname} нет на сервере {guild.name}")
+    endedOrNote=True
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} запущен!")
-    sync_roles.start()  # synchronize
+    await configurated_logging(level=logging.INFO)
+    log.info(f"{bot.user} запущен!")
+    await sync_roles()  # Синхронизация
 
 
-@tasks.loop(seconds=15)
-async def sync_roles():
-    print("Старт проверки и синхронизации")
-
-    guilds = [bot.get_guild(g) for g in guildslist if bot.get_guild(g)]
-
-    for guild in guilds:
-        for member in guild.members:
-            print(f"Чекинг {member.name} на {guild.name}")
-            for r in member.roles:
-                print(f"   - {r.name}")
-            if member.bot:
-                continue
-
-            # Taking ALL roles from servers
-            user_roles_names = set()
-            for g in guilds:
-                m = g.get_member(member.id)
-                if not m:
-                    continue
-                for r in m.roles:
-                    if r.name not in ["@everyone", "BotRole"]:#trash roles are going from list of roles on synchronize
-                        user_roles_names.add(r.name)
-
-
-            # Cicle for all servers on list and adding roles
-            for g in guilds:
-                target_member = g.get_member(member.id)
-                if not target_member:
-                    continue
-
-                current_roles = [r.name for r in target_member.roles]
-
-                for role_name in user_roles_names:
-                    if role_name not in current_roles:
-                        role = discord.utils.get(g.roles, name=role_name)
-                        if role:
-                            try:
-                                await target_member.add_roles(role)
-                                print(f"{target_member.name} получил {role_name} на сервере: {g.name}")
-                            except discord.Forbidden:
-                                print(f"Нет прав выдать {role_name} на {g.name}")
+if endedOrNote==True:
+    @bot.event
+    async def on_member_update(before: discord.Member, after: discord.Member):
+        await configurated_logging(level=logging.INFO)
+        guilds = [bot.get_guild(g) for g in guildslist if bot.get_guild(g)]
+        memberRoles=after.roles
+        for role in memberRoles:
+            rname=role.name
+            if rname=='падаван':
+                for g in guilds:
+                    log.info(f'Сервер:{g}')
+                    log.info(f'Имя роли:{rname}')
+                    user=g.get_member(after.id)
+                    targetrole = discord.utils.get(g.roles , name=rname)
+                    print(targetrole)
+                    if targetrole:
+                        try:
+                            await user.add_roles(targetrole)
+                            log.info(f'{user} получил роль:{targetrole.name}')
+                        except discord.Forbidden:
+                            log.error(f"Нет прав выдать {targetrole.name}")
                         else:
-                            print(f"{role_name} нет на сервере {g.name}")
+                            if targetrole == None:
+                                log.fatal(f"{rname} нет на сервере")
 
-#Giving role with synchronize
+
+#Выдать роли на всех сервах по названию
 @bot.slash_command(description="give a role with synchronyzinc to other servers")
 @discord.ext.commands.has_permissions(manage_messages=True)
 async def set_role(ctx,user: discord.Member , * , rolename):
+    await configurated_logging(level=logging.INFO)
+    guildslist=os.getenv('guilds')
     for server in guildslist:
         targetguild = bot.get_guild(server)
-        print(server)
+        log.info(f'Сервер на котором проходит операция:{server}')
         targetrole = discord.utils.get(targetguild.roles , name=rolename)
         targetmember = targetguild.get_member(user.id)
         await targetmember.add_roles(targetrole)
+        log.info(f'Бот успешно выдал роль {targetrole}, на серверах {guildslist} пользователю {targetmember}')
         await ctx.response.send_message(f"Бот {bot.user} совершил попытку изменить роль {targetmember}-у на {targetrole} на серверах {guildslist}",ephemeral=True)
 
-#Remove role with synchronize
+#Лишить роли на всех сервах по названию 
 @bot.slash_command(description="remove a role with synchronyzinc to other servers")
 @discord.ext.commands.has_permissions(manage_messages=True)
 async def remove_role(ctx,user: discord.Member , * , rolename):
+    await configurated_logging(level=logging.INFO)
     for server in guildslist:
         targetguild = bot.get_guild(server)
-        print(server)
+        log.info(f'Сервер на котором проходит операция:{server}')
         targetrole = discord.utils.get(targetguild.roles , name=rolename)
         targetmember = targetguild.get_member(user.id)
         await targetmember.remove_roles(targetrole)
+    log.info(f'Бот успешно удалил роль {targetrole}, на серверах {guildslist} пользователю {targetmember}')
     await ctx.response.send_message(f"Бот {bot.user} совершил попытку удалить роль у {targetmember} на сервере {guildslist}",ephemeral=True)
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#synchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronizesynchronize....i hate this word
